2b328caa026420af4bc2a8a8aa15d3fbc6229948
AndreaTrento95 <trentora95@gmail.com>
2021-04-20 16:50:38 +0200

    svolgimento

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,36 @@
 $(document).ready(function(){
 
+    // 
+    $('.fa-chevron-right').click(switchRight);
+    $('.fa-chevron-left').click(switchLeft);
+
+    // ------ function --------
+    function switchRight(){
+        // creo una var per individuare l'immagine che ha la classe active
+        var img_active = $('.wrapper .images img.active');
+
+        // rimuovo la classe active dall'immagine
+        img_active.removeClass('active');
+        
+        console.log(img_active);
+
+        if(img_active.length === 0){
+            img_active.first().addClass('active');
+        }else{
+            // do la classe active all'elemento (img) successivo
+            img_active.next('img').addClass('active'); 
+        }
+        
+    }
+
+    function switchLeft(){
+        // creo una varaibile per individuare l'immagine che ha la classe active
+        var img_active = $('.wrapper .images img.active');
+        // rimuovo la classe active dall'immagine
+        img_active.removeClass('active');
+        // do la classe active all'elemento (img) precedente
+        img_active.prev('img').addClass('active');
+    }
 
 
 });
